5f527cc4587bdc8ff4fd5b43ead1220196229be3
rajandangi <rajandangi649@gmail.com>
2024-05-27 11:57:15 +0930

    Added new theme

---
+++ theme/rajan/lang/en/theme_rajan.php
@@ -0,0 +1,12 @@
+<?php
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();
+
+// A description shown in the admin theme selector.                                                                                 
+$string['choosereadme'] = 'Theme rajan is a child theme of Boost. It is designed to be a clean, simple and modern theme that is easy to use and customize.';
+// The name of our plugin.                                                                                                          
+$string['pluginname'] = 'Rajan Theme';
+// We need to include a lang string for each block region.                                                                          
+$string['region-side-pre'] = 'Right';
